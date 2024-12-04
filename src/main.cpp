4d5a7729435f880f77f4661d7458c5e633b7fbd6
LucasLiuXinqi <41111689+LucasLiuXinqi@users.noreply.github.com>
2024-12-04 15:21:03 -0500

    add acc

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,56 @@
 #include <Arduino.h>
 #include <Adafruit_CircuitPlayground.h>
+#include <SPI.h>
 
-void BlueLoad();
+// void BlueLoad();
 void GreenBlink();
 void ButtonInit();
 void RedBlink();
+void PixelsInit();
+void AccInit();
+void ReadAcc();
+void CollectAcc();
+
+int16_t AccData[60][3]; // 2D array: 60 rows, 3 columns (X, Y, Z)
 
 void setup() {
-    CircuitPlayground.begin();    // Board init
+    PixelsInit();
     ButtonInit();
+    AccInit();
 }
 
 void loop() {
-    CircuitPlayground.clearPixels();
+    // CircuitPlayground.clearPixels();
     // If LB pressed, start recording
     if ((PIND & (1<<PD4)) != 0){
-        BlueLoad();
+        CollectAcc();
+        for (int i = 0; i < 60; i++) {
+            Serial.print("Sample "); Serial.print(i); Serial.print(": ");
+            Serial.print("X = "); Serial.print(AccData[i][0]);
+            Serial.print(", Y = "); Serial.print(AccData[i][1]);
+            Serial.print(", Z = "); Serial.println(AccData[i][2]);
+        }
+        // BlueLoad();
         GreenBlink();
     }
 
-    // If RB pressed, start comparing
-    if ((PINF & (1<<PF6)) != 0){
-        BlueLoad();
-        RedBlink();
-    }
+    // // If RB pressed, start comparing
+    // if ((PINF & (1<<PF6)) != 0){
+    //     BlueLoad();
+    //     RedBlink();
+    // }
+
+
 }
 
 
-void BlueLoad(){
-    CircuitPlayground.clearPixels();
-    for(int8_t i = 0; i < 10; i++){
-        CircuitPlayground.setPixelColor(i, 0, 0, 255);
-        delay(300);
-    }
-}
+// void BlueLoad(){
+//     CircuitPlayground.clearPixels();
+//     for(int8_t i = 0; i < 10; i++){
+//         CircuitPlayground.setPixelColor(i, 0, 0, 255);
+//         delay(300);
+//     }
+// }
 
 void GreenBlink(){
     CircuitPlayground.clearPixels();
@@ -41,6 +58,8 @@ void GreenBlink(){
         for(int8_t i = 0; i < 10; i++){
             CircuitPlayground.setPixelColor(i, 0, 255, 0);
         }
+        delay(100);
+        CircuitPlayground.clearPixels();
         delay(100);
     }
 
@@ -53,6 +72,8 @@ void RedBlink(){
             CircuitPlayground.setPixelColor(i, 255, 0, 0);
         }
         delay(100);
+        CircuitPlayground.clearPixels();
+        delay(100);
     }
 
 }
@@ -61,4 +82,68 @@ void ButtonInit(){
     // Left Button: PD4, Right Button PF6
     DDRD &= ~(1<<PD4);
     DDRF &= ~(1<<PF6);
+}
+
+void AccInit(){
+    DDRB |= (1<<PIN4);
+    SPI.begin();
+
+    // Initialize CTRL_REG1 (0x20) to enable axes and set data rate
+    PORTB &= ~(1 << PIN4);
+    SPI.transfer(0x20); // Write to CTRL_REG1
+    SPI.transfer(0x37); // Enable all axes, set 20Hz data rate
+    PORTB |= (1 << PIN4);
+
+    // Initialize CTRL_REG4 (0x23) for data alignment
+    PORTB &= ~(1 << PIN4);
+    SPI.transfer(0x23); // Write to CTRL_REG4
+    SPI.transfer(0x00); // ±2g, continuous update
+    PORTB |= (1 << PIN4);
+}
+
+void PixelsInit(){
+    CircuitPlayground.begin();    // Board init
+}
+
+void ReadAcc(int16_t *x, int16_t *y, int16_t *z) {
+    uint8_t buffer[6];
+
+    // Set chip select low
+    PORTB &= ~(1 << PIN4);
+
+    // Send the address of OUT_X_L (start of X, Y, Z data) with auto-increment
+    SPI.transfer(0x28 | (1 << 7) | (1 << 6)); // Read, auto-increment
+
+    // Read 6 bytes (X_L, X_H, Y_L, Y_H, Z_L, Z_H)
+    for (int i = 0; i < 6; i++) {
+        buffer[i] = SPI.transfer(0xFF); // Send dummy byte to read
+    }
+
+    // Set chip select high
+    PORTB |= (1 << PIN4);
+
+    // Combine high and low bytes for each axis
+    *x = (int16_t)(buffer[1] << 8 | buffer[0]);
+    *y = (int16_t)(buffer[3] << 8 | buffer[2]);
+    *z = (int16_t)(buffer[5] << 8 | buffer[4]);
+}
+
+void CollectAcc() {
+    for (int i = 0; i < 60; i++) {
+        // Read X, Y, Z values
+        int16_t x, y, z;
+        ReadAcc(&x, &y, &z); // Pass by reference to get the values
+
+        // Store values in the buffer
+        AccData[i][0] = x; // X-axis
+        AccData[i][1] = y; // Y-axis
+        AccData[i][2] = z; // Z-axis
+
+        // Wait for the next sample (50 ms for 20 Hz sampling rate)
+
+        CircuitPlayground.setPixelColor(i/6, 0, 0, 255);
+
+        delay(50);
+
+    }
 }
